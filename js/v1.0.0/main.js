73a5661b351e1989be6c4e2a01771cef34640e0f
Kuhle Matolengwe <kuhlematolengwe@gmail.com>
2026-02-07 12:36:23 +0200

    Update main.js

--- js/v1.0.0/main.js
+++ js/v1.0.0/main.js
@@ -1,12 +1,8 @@
 //The function _debug any will take any function with an unlimited number of passed arguments can be passed.
-function _debug(_function, ...params)
-{
-     try
-     {
+function _debug(_function, ...params){
+     try{
           _function(...params);
-     }
-     catch(err)
-     { 
+     }catch(err){ 
           alert(err);
      }
 }
